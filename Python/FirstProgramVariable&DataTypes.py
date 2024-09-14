4fc86a7bf29f6b9a7d0c8951f5da01186efcccf9
navjyotisharma1 <navjyotisharma477@gmail.com>
2024-09-14 23:32:48 +0530

    Declared Variable & string type value

--- Python/FirstProgramVariable&DataTypes.py
+++ Python/FirstProgramVariable&DataTypes.py
@@ -1,3 +1,33 @@
-age = int(input("age : "))
-vote = ("yes", "no") [age < 18]
-print(vote)
+'''
+Variables
+A variable is a name given to a memory location in a program.
+
+'''
+name = "Navjyoti Sharma"
+age = 29
+price = 25.58
+print("my name is : ", name)
+print("my age is :", age)
+print("price of tomato is :", price)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+
+'''
+Data Types
+Integer
+String
+Float
+Boolean
+None
+'''
+name1 = "pk"
+name2 = "mk"
+name3 = "ck"
+
+print(name1)
+print(name2)
+print(name3)
